0d90c66b7ecfc9086091c3560c152d0efb3ddc31
Alex Jokela <alex@camulus.com>
2026-06-10 19:08:53 -0500

    Bump to v0.4.1

--- tests/test_stdlib.c
+++ tests/test_stdlib.c
@@ -1242,7 +1242,7 @@ static void test_lat_eval_version(void) {
     ASSERT_OUTPUT("fn main() {\n"
                   "    print(version())\n"
                   "}\n",
-                  "0.4.0");
+                  "0.4.1");
 }
 
 /* ======================================================================
@@ -5054,7 +5054,7 @@ static void test_triple_multiline_interpolation(void) {
                   "    \"\"\"\n"
                   "    print(s)\n"
                   "}\n",
-                  "Hello, Lattice!\nVersion 0.4.0");
+                  "Hello, Lattice!\nVersion 0.4.1");
 }
 
 static void test_triple_embedded_quotes(void) {
